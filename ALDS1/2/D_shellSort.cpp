3d3236077bdc4533fcb87efc0ef92c8b84dbaf65
ta121443 <ryo_uchi4_20@outlook.jp>
2022-07-02 20:41:56 +0900

    add

--- ALDS1/2/D_shellSort.cpp
+++ ALDS1/2/D_shellSort.cpp
@@ -1,13 +1,7 @@
+// 出来てない！！悔しい！！！！
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for(int i=0;i<(int)(n);i++)
 #define rep2(i, s, n) for(int i=(s);i<(int)(n);i++)
 #define reprev(i, n) for(int i=(int(n-1));i>=0;i--)
 #define reprev2(i, s, e) for(int i=(int(s));i>=(int)(e);i--)
-
-int main(){
-    int n;
-    cin >> n;
-    vector<int> A(n);
-    rep(i, n) cin >> A[i];
-}
